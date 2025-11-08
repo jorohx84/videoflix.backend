4d43a787e6a5cb1de72dd1eb3bac296618e1e5d2
Johannes Roth <jrothx@icloud.com>
2025-11-08 08:15:04 +0100

    save

--- auth_app/api/serializers.py
+++ auth_app/api/serializers.py
@@ -1,28 +1,3 @@
-# from django.contrib.auth.models import AbstractUser
-# from rest_framework import serializers
-# from ..models import CustomUser
-
-
-
-# class RegisterSerializer(serializers.ModelSerializer):
-#     confirmed_password = serializers.CharField(write_only=True)
-
-#     class Meta:
-#         model = CustomUser  # direkt AbstractUser
-#         fields = ('email', 'password', 'confirmed_password')
-#         extra_kwargs = {'password': {'write_only': True}}
-
-#     def validate(self, data):
-#         if data['password'] != data['confirmed_password']:
-#             raise serializers.ValidationError("Passwords do not match")
-#         return data
-
-#     def create(self, validated_data):
-#         validated_data.pop('confirmed_password')
-#         # username auf email setzen, da AbstractUser ein username Feld hat
-#         validated_data['username'] = validated_data['email']
-#         return CustomUser.objects.create_user(**validated_data)
-
 from django.contrib.auth.models import User
 from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
@@ -37,7 +12,7 @@ class RegisterSerializer(serializers.ModelSerializer):
         extra_kwargs = {'password': {'write_only': True}}
 
     def validate(self, data):
-        # Passwortbestätigung prüfen
+ 
         if data['password'] != data['confirmed_password']:
             raise serializers.ValidationError("Passwords do not match")
         return data
@@ -45,14 +20,13 @@ class RegisterSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         validated_data.pop('confirmed_password')
 
-        # ⚠️ Trick: username = email, da das Standardmodell username verlangt
         user = User.objects.create_user(
-            username=validated_data['email'],   # <- Hier passiert die Magie!
+            username=validated_data['email'],  
             email=validated_data['email'],
             password=validated_data['password']
         )
 
-        # Optional: direkt deaktivieren bis zur Aktivierung per E-Mail
+     
         user.is_active = False
         user.save()
 
@@ -90,47 +64,7 @@ class CustomTokenObtainSerializer(TokenObtainPairSerializer):
         if not user.check_password(password):
             raise serializers.ValidationError("Invalid password or email")
         
-        # data = super().validate({
-        #     self.username_field:getattr(user, self.username_field),
-        #     "password": password
-        # })
         attrs["username"]=user.username
         data = super().validate(attrs)
         return data
     
-
-# class CustomTokenObtainSerializer(TokenObtainPairSerializer):
-#     email = serializers.EmailField()
-#     password = serializers.CharField(write_only=True)
-
-#     # Überschreibe das Feld, das SimpleJWT für den Login erwartet
-#     username_field = 'email'
-
-#     def __init__(self, *args, **kwargs):
-#         super().__init__(*args, **kwargs)
-#         # Entferne das automatisch hinzugefügte username-Feld
-#         if 'username' in self.fields:
-#             self.fields.pop('username')
-#         # Füge das email-Feld als username_field hinzu
-#         self.fields[self.username_field] = self.fields.pop('email')
-
-#     def validate(self, attrs):
-#         email = attrs['email']
-#         password = attrs['password']
-
-#         try:
-#             user = User.objects.get(email=email)
-#         except User.DoesNotExist:
-#             raise serializers.ValidationError("Invalid email or password")
-
-#         if not user.check_password(password):
-#             raise serializers.ValidationError("Invalid email or password")
-
-#         # SimpleJWT erwartet hier das Klartext-Passwort
-#         data = super().validate({
-#             self.username_field: email,
-#             'password': password
-#         })
-
-#         self.user = user
-#         return data
--- auth_app/api/views.py
+++ auth_app/api/views.py
@@ -6,26 +6,27 @@ from django_rq import enqueue
 from .utils import send_activation_email, activate_user
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth.models import User
+
 class RegisterView(APIView):
     def post(self, request):
         serializer = RegisterSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
-        # JWT erstellen
+    
         refresh = RefreshToken.for_user(user)
 
-        # Asynchron E-Mail versenden
+        
         enqueue(send_activation_email, user.email, str(refresh.access_token))
-        # send_activation_email(user.email, "TEST-TOKEN")
+      
         response = Response({
             "user": {
                 "id": user.id,
                 "email": user.email
             },
-            "token": str(refresh.access_token)  # nur Info
+            "token": str(refresh.access_token)  
         }, status=status.HTTP_201_CREATED)
 
-        # Token als HttpOnly Cookie setzen
+      
         response.set_cookie(
             key='access_token',
             value=str(refresh.access_token),
