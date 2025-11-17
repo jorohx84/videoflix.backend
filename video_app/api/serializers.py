2f4903688220cb28dd4f82322447b0a55d8f682f
Johannes Roth <jrothx@icloud.com>
2025-11-17 21:20:41 +0100

    save

--- video_app/api/serializers.py
+++ video_app/api/serializers.py
@@ -1,4 +1,3 @@
-
 from rest_framework import serializers
 from django.conf import settings
 from ..models import Video
@@ -11,8 +10,20 @@ class VideoSerializer(serializers.ModelSerializer):
         model = Video
         fields = ('id', 'title', 'description', 'category', 'created_at', 'thumbnail_url')
 
+    # def get_thumbnail_url(self, obj):
+    #     thumbnail_path = os.path.join(settings.MEDIA_ROOT, "thumbnails", f"{obj.id}.jpg")
+    #     if os.path.exists(thumbnail_path):
+    #         return settings.MEDIA_URL + f"thumbnails/{obj.id}.jpg"
+    #     return None
+
     def get_thumbnail_url(self, obj):
         thumbnail_path = os.path.join(settings.MEDIA_ROOT, "thumbnails", f"{obj.id}.jpg")
         if os.path.exists(thumbnail_path):
-            return settings.MEDIA_URL + f"thumbnails/{obj.id}.jpg"
+            request = self.context.get('request')
+            if request:
+                # absolute URL inkl. Backend-Host + Port
+                return request.build_absolute_uri(settings.MEDIA_URL + f"thumbnails/{obj.id}.jpg")
+            else:
+                # Fallback: nur relative URL
+                return settings.MEDIA_URL + f"thumbnails/{obj.id}.jpg"
         return None
